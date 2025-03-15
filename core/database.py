ed3aacd6168d64950fc57c18a73753b92f516c55
dmytro <dmysamo@gmail.com>
2025-03-15 11:53:46 +0200

    feat: add user model

--- core/database.py
+++ core/database.py
@@ -1,11 +1,15 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from core.settings import settings
 
 
 engine = create_async_engine(settings.database_url, echo=settings.debug)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-Base = declarative_base()
+
+
+class Base(DeclarativeBase):
+    pass
+
 
 async def get_db():
     async with SessionLocal() as db:
---
+++ src/users/models.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, DateTime, func
+
+from core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(255), unique=True, nullable=False, index=True
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+
+    def __repr__(self) -> str:
+        return f"name: {self.name}, email: {self.email}, created_at: {self.created_at}"
