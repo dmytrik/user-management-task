06a9f4d2838df593efd97417f062476a139394d0
dmytro <dmysamo@gmail.com>
2025-03-15 13:29:08 +0200

    feat: add schemas and validators

---
+++ src/users/schemas.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, field_validator
+
+from src.users.validators import validate_name, validate_email
+
+
+class UserBaseSchema(BaseModel):
+    name: str
+    email: EmailStr
+
+    @field_validator("name")
+    @classmethod
+    def validate_email(cls, value):
+        return validate_name(value)
+
+    @field_validator("email")
+    @classmethod
+    def validate_password(cls, value):
+        return validate_email(value)
+
+
+class UserCreateRequestSchema(UserBaseSchema):
+    pass
+
+
+class UserCreateResponseSchema(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserUpdateRequestSchema(UserBaseSchema):
+    name: Optional[str]
+    email: Optional[EmailStr]
+
+
+class UserUpdateResponseSchema(UserCreateResponseSchema):
+    pass
---
+++ src/users/validators.py
@@ -0,0 +1,23 @@
+import email_validator
+from pydantic import ValidationError
+
+
+def validate_name(name: str) -> str:
+    normalized_name = name.strip();
+    if len(normalized_name) < 2:
+        raise ValidationError("Name must be at least 2 characters long")
+    elif any(char.isdigit() for char in normalized_name):
+        raise ValidationError("Name cannot contain numbers")
+    return normalized_name
+
+
+def validate_email(user_email: str) -> str:
+    try:
+        email_info = email_validator.validate_email(
+            user_email, check_deliverability=False
+        )
+        email = email_info.normalized
+    except email_validator.EmailNotValidError as error:
+        raise ValidationError(str(error))
+    else:
+        return email
